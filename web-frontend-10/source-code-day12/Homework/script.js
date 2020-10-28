94e212a7f1cb36933d50fe72e57f769ddc3b7b20
unknown <wwf.1703@gmail.com>
2020-10-29 01:56:02 +0700

    upload

--- web-frontend-10/source-code-day12/Homework/script.js
+++ web-frontend-10/source-code-day12/Homework/script.js
@@ -1,4 +1,14 @@
 // js
+
+document.write("<h2>1. Cho một mảng số nguyên. Viết hàm kiểm tra nếu trong mảng có phần tử âm thì thay thế nó bằng 0.</h2>")
+document.write('<p>Dùng hàm " checkNegative() " tham số truyền vào là 1 mảng </p>')
+document.write("<h2>2. Viết hàm tìm số nhỏ nhất và lớn nhất trong mảng số nguyên.</h2>")
+document.write('<p>Dùng hàm " minMax() " tham số truyền vào là 1 mảng </p>')
+document.write("<h2>3. Viết hàm trả về vị trí đầu tiên của số lẻ, vị trí cuối cùng của số chẵn trong mảng bất kì.</h2>")
+document.write('<p>Dùng hàm " find() " tham số truyền vào là 1 mảng </p>')
+
+
+
 // Hàm kiểm tra nếu trong mảng có phần tử âm thì thay thế nó bằng 0
 function checkNegative(arr){
     for(let i = 0; i < arr.length; i++){
@@ -6,7 +16,7 @@ function checkNegative(arr){
             arr[i] = 0
         }
     }
-    console.log(`New Array ${arr}`)
+    console.log(`New Array : [${arr}]`)
 }
 
 // Hàm tìm số nhỏ nhất và lớn nhất trong mảng số nguyên
