33761d047a85bc8cc7516fe7182fa81356d7327e
unknown <wwf.1703@gmail.com>
2020-11-23 15:00:12 +0700

    upload

--- web-frontend-10/Web-nang-cao/source-code-day5/homework/script.js
+++ web-frontend-10/Web-nang-cao/source-code-day5/homework/script.js
@@ -1,6 +1,8 @@
 // JS
 
-let menu = document.getElementsByClassName("menu-mobile")[0]
+let menu = document.getElementsByClassName("menu")[0]
+
+// let ul_list = document.getElementsByTagName("ul")[0]
 
 window.addEventListener("resize", _resize)
 
@@ -9,8 +11,6 @@ function _resize() {
     let currentWidth = window.innerWidth
 
     if (currentWidth > 576) {
-        menu.style.display = "none"
-    } else if (currentWidth == "none") {
         menu.style.display = "flex"
     }
 }
@@ -20,10 +20,10 @@ function hamburger() {
     let comp = window.getComputedStyle(menu)
     let val = comp.getPropertyValue("display")
 
-    if (val == "flex") {
+    if (val === "flex") {
         menu.style.display = "none"
-    } else if (val == "none") {
+    } else if (val === "none") {
         menu.style.display = "flex"
     }
-}
 
+}
