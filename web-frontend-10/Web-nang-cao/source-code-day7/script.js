932212ea364811dc1f6c649e53a01df3f7549d7f
unknown <wwf.1703@gmail.com>
2020-12-05 22:43:51 +0700

    update

--- web-frontend-10/Web-nang-cao/source-code-day7/script.js
+++ web-frontend-10/Web-nang-cao/source-code-day7/script.js
@@ -4,12 +4,14 @@
 
 let bg = document.getElementsByClassName("banner-background")[0]
 
-
-
-let data = window.getComputedStyle(bg)
-let res = data.getPropertyValue("height")
+let curent_width = window.innerWidth
 
 let curent_height = window.innerHeight
 
-bg.style.height = (curent_height - 100) + "px"
-console.log(curent_height)
+if (curent_width > 992) {
+    bg.style.height = (curent_height - 100) + "px"
+    console.log(curent_height)
+}else if(curent_width <= 992){
+    bg.style.height = (curent_height - 63) + "px"
+}
+
