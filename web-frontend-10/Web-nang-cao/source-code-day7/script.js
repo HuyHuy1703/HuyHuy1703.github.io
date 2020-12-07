cb857a361bea316597c60a384dd16e40cd38a641
unknown <wwf.1703@gmail.com>
2020-12-07 11:55:24 +0700

    update

--- web-frontend-10/Web-nang-cao/source-code-day7/script.js
+++ web-frontend-10/Web-nang-cao/source-code-day7/script.js
@@ -53,3 +53,44 @@ function _resize() {
     }
 }
 // End
+
+// Slick Slider
+$('.responsive').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          arrows: false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: false,
+          autoplay: true
+        }
+      }
+    ]
+  });
